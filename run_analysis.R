0c9850d484005315160f8443c4bde4419f0f6977
ignaciomsarmiento <ignaciomsarmiento@gmail.com>
2014-07-15 15:35:24 -0500

    all file

--- run_analysis.R
+++ run_analysis.R
@@ -9,20 +9,20 @@ if (!file.exists("data")){
 }
 
 
-download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile="data/temp.zip")
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile="data/data.zip")
 dateDownloaded <- date()
 
-file_ls <- as.character(unzip("data/temp.zip", list = TRUE)$Name)
+file_ls <- as.character(unzip("data/data.zip", list = TRUE)$Name)
 
 #files
 
-X_test<-read.table(unzip("data/temp.zip", files="UCI HAR Dataset/test/X_test.txt"))
+X_test<-read.table(unzip("data/data.zip", files="UCI HAR Dataset/test/X_test.txt"))
 
-y_test<-read.table(unzip("data/temp.zip", files="UCI HAR Dataset/test/y_test.txt"))
+y_test<-read.table(unzip("data/data.zip", files="UCI HAR Dataset/test/y_test.txt"))
 
-X_train<-read.table(unzip("data/temp.zip", files="UCI HAR Dataset/train/X_train.txt"))
+X_train<-read.table(unzip("data/data.zip", files="UCI HAR Dataset/train/X_train.txt"))
 
-y_train<-read.table(unzip("data/temp.zip", files="UCI HAR Dataset/train/y_train.txt"))
+y_train<-read.table(unzip("data/data.zip", files="UCI HAR Dataset/train/y_train.txt"))
 
 
 
